9c608c703103ecebaeb06b1e9a812dc318fd416b
Ayush Sinha <ayush40451k@gmail.com>
2024-01-24 23:59:25 +0530

    quiz game

---
+++ dat.py
@@ -0,0 +1,8 @@
+question_data = [
+    {"text":"delhi is capital of india","answer": "True"},
+    {"text":"Mumbai is capital of india", "answer" : "Flase"},
+    {"text":"benglaluru is capital of india" ,  "answer" : "Flase"},
+    {"text":"benglaure is capital of india" ,  "answer" : "True"}
+
+
+]
---
+++ main.py
@@ -0,0 +1,22 @@
+from Question_data import Question
+from dat import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
+    
+    
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questioon():
+    quiz.next_question()
+    
+    
+    
+print("You've completed the quiz ")
+print(f"your final score was : {quiz.score}/{quiz.question_number}")
+    
